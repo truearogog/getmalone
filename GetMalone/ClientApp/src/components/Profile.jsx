a5dad95b8ca8331bc86fd29da5388675779a1c88
C-Coretex <valerash2@gmail.com>
2022-06-17 18:13:16 +0300

    Added styled-components

--- GetMalone/ClientApp/src/components/Profile.jsx
+++ GetMalone/ClientApp/src/components/Profile.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import styled from 'styled-components'
 //import { variables } from '../variables';
 //import { v4 as uuidv4 } from 'uuid';
 
 export function Profile() {
 	return (
 		<div>
+			<MyForm />
 			Form
-		</div >
+		</div>
 	);
 }
 
+const MyForm = styled.div`
+  background-color: grey;
+	height: 100px;
+	width: 20px;
+`;
