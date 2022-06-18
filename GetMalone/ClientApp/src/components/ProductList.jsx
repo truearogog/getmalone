8d9dce5730e8faa861dde1daca74aea59ffece4e
sakuraaah <100445166+sakuraaah@users.noreply.github.com>
2022-06-18 22:00:49 +0300

    Created ProductList

---
+++ GetMalone/ClientApp/src/components/ProductList.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import styled from 'styled-components'
+//import { variables } from '../variables';
+import { v4 as uuidv4 } from 'uuid';
+
+const id = 0;
+
+export function ProductList({ data }) {
+	return (
+		<ProductListWrapper>
+			{data.map(data => <ProductItem key={uuidv4()} data={data}/>)}
+		</ProductListWrapper>
+	)
+}
+const ProductListWrapper = styled.div`
+	background: rgba(100, 100, 100, 0.8);
+	display: flex;
+    flex-wrap: wrap;
+    width: 100%;
+	margin-top: 25px;
+`;
+function ProductItem({ data }) {
+	return (
+		<ProductItemWrapper>
+			<Image src={require('../images/product-pictures/'+id+'.png')} />
+			<Title>{data.name}</Title>
+			<Description>"{data.description}"</Description>
+			<Price>{data.cost}$</Price>
+			<Info>
+				<p>Type: <b>{data.type}</b></p> 
+				<p>Best Before: <b>{data.date}</b></p> 
+				<p>Seller: <b>{data.seller}</b></p>
+			</Info>
+		</ProductItemWrapper>
+	)
+}
+const ProductItemWrapper = styled.div`
+	background: rgba(100, 100, 100, 0.8);
+	width: 25%;
+	text-align: center;
+	padding-top: 30px;
+  padding-bottom: 20px;
+	line-height: 10px;
+`;
+const Image = styled.img`
+	object-fit: cover;
+	height: 150px;
+	width: 150px;
+	margin: 0 auto;
+	border-radius: 50%;
+`;
+const Title = styled.p`
+	padding-top: 10px;
+	font-weight: 600;
+	font-size: 20px;
+`;
+const Description = styled.p`
+	font-size: 15px;
+	font-style: italic;
+`;
+const Price = styled.p`
+	padding-top: 5px;
+  padding-bottom: 5px;
+	font-size: 26px;
+`;
+const Info = styled.div`
+	font-family: 'Nanum Gothic', sans-serif;
+	font-size: 16px;
+`;
--- GetMalone/ClientApp/src/components/UserProfile.jsx
+++ GetMalone/ClientApp/src/components/UserProfile.jsx
@@ -1,9 +1,53 @@
 import React from 'react';
 import styled from 'styled-components'
+import { ProductList } from './ProductList';
 //import { variables } from '../variables';
 //import { v4 as uuidv4 } from 'uuid';
 
 const id = 0;
+
+let data = [
+  {
+    name: "Apple",
+    description: "best apples",
+    cost: 0.99,
+    type: "Fruits",
+    date: "2022-06-23",
+    seller: 2
+  },
+  {
+    name: "Potato",
+    description: "nice and cheap",
+    cost: 0.39,
+    type: "Vegetables",
+    date: "2022-07-13",
+    seller: 3
+  },
+  {
+    name: "Cucumber",
+    description: "fresh and homemade",
+    cost: 0.49,
+    type: "Vegetables",
+    date: "2022-07-19",
+    seller: 2
+  },
+  {
+    name: "Pear",
+    description: "yummy",
+    cost: 1.29,
+    type: "Fruits",
+    date: "2022-05-14",
+    seller: 1
+  },
+  {
+    name: "Beetroot",
+    description: null,
+    cost: 0.79,
+    type: "Vegetables",
+    date: "2022-09-23",
+    seller: 8
+  },
+]
 
 export function UserProfile() {
 	return (
@@ -110,45 +154,18 @@ function RecommendedProducts() {
 			<ListTitle>
 				Recommended products
 			</ListTitle>
-			<ProductList />
+			<ProductList data={data}/>
 		</RecommendedProductsWrapper>
 	)
 }
 const RecommendedProductsWrapper = styled.div`
-	padding-top: 60px;
+	padding-top: 90px;
+	width: 90%;
+	margin: 0 auto;
 `;
 const ListTitle = styled.div`
 	padding-bottom: 20px;
 	font-size: 40px;
 	font-weight: 600;
 	text-align: center;
-`;
-function ProductList() {
-	return (
-		<ProductListWrapper>
-			<ProductItem />
-			<ProductItem />
-			<ProductItem />
-			<ProductItem />
-			<ProductItem />
-			<ProductItem />
-		</ProductListWrapper>
-	)
-}
-const ProductListWrapper = styled.div`
-	background: rgba(100, 100, 100, 0.8);
-	display: flex;
-    flex-wrap: wrap;
-    width: 100%;
-`;
-function ProductItem() {
-	return (
-		<ProductItemWrapper>
-			product
-		</ProductItemWrapper>
-	)
-}
-const ProductItemWrapper = styled.div`
-	background: rgba(100, 100, 100, 0.8);
-	width: 25%;
 `;
