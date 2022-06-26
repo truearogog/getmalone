f749d3c14f804d6d0eed5345042d1290c38e8cb7
sakuraaah <100445166+sakuraaah@users.noreply.github.com>
2022-06-26 23:38:01 +0300

    Update Product.jsx

--- GetMalone/ClientApp/src/components/ProductPage/Product.jsx
+++ GetMalone/ClientApp/src/components/ProductPage/Product.jsx
@@ -7,6 +7,11 @@ export function Product({ handlePageChange, productid }) {
 
   const [product, setProductById] = useState('')
   const [error, setError] = useState('')
+  const [name, setName] = useState('')
+	const [description, setDescription] = useState('')
+	const [categories, setCategories] = useState([])
+	const [priceEuro, setPriceEuro] = useState('')
+	const [currentCategory, setCurrentCategory] = useState({ id: 1 })
 
   async function getProductById() {
     const formData = { id: productid }
@@ -15,10 +20,8 @@ export function Product({ handlePageChange, productid }) {
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify(formData)
     }
-    console.log(requestOptions)
 		try {
 			const response = await fetch(variables.API_URL + 'product/get', requestOptions);
-      console.log(response);
 			if (!response.ok) throw new Error(response.statusText, requestOptions)
 
 			const data = await response.json();
@@ -32,17 +35,38 @@ export function Product({ handlePageChange, productid }) {
 		}
 	}
 
-	useEffect(() => {
+  useEffect(() => {
 		getProductById()
 	}, [])
 
-	const [name, setName] = useState('')
-	const [description, setDescription] = useState('')
-	const [categories, setCategories] = useState([])
-	const [priceEuro, setPriceEuro] = useState('')
-	const [currentCategory, setCurrentCategory] = useState({ id: 1 })
+	useEffect(() => {
+    getCategories()
+	}, [product])
 
-	async function handleSubmit(e) {
+	async function getCategories() {
+		try {
+			const response = await fetch(variables.API_URL + 'product/allcategories');
+			if (!response.ok) throw new Error(response.statusText)
+
+			const data = await response.json();
+			if (data.success == false) throw new Error(data.error)
+
+			setCategories(data.data);
+
+      console.log(data.data)
+      console.log(product.category.id)
+      console.log(data.data.some(category => category.id === product.category.id))
+      
+      typeof(product.category.id) !== 'undefined' ? setCurrentCategory(data.data.find(category => category.id === product.category.id)) : setCurrentCategory(data.data[0]);
+			
+		}
+		catch (err) {
+			console.log(err)
+			setError(err)
+		}
+	}
+
+  async function handleSubmit(e) {
 		e.preventDefault()
 
 		const formData = { name: name, description: description, categoryId: currentCategory.id, priceEuro: priceEuro }
@@ -69,29 +93,6 @@ export function Product({ handlePageChange, productid }) {
 		}
 	}
 
-
-	async function getCategories() {
-		try {
-			const response = await fetch(variables.API_URL + 'product/allcategories');
-			if (!response.ok) throw new Error(response.statusText)
-
-			const data = await response.json();
-			if (data.success == false) throw new Error(data.error)
-
-			setCategories(data.data);
-
-			setCurrentCategory(data.data[0]);
-		}
-		catch (err) {
-			console.log(err)
-			setError(err)
-		}
-	}
-
-	useEffect(() => {
-		getCategories()
-	}, [])
-
 	function onChange(e, setFunction, maxSymbols = 2) {
 		if (e.target.value !== '' && e.target.value.length - 1 < maxSymbols) {
 			if (!isNaN(e.target.value)) {
@@ -104,12 +105,12 @@ export function Product({ handlePageChange, productid }) {
     <Container>
       <FormContainer>
 				<FormFields>
-          {/* Category:
+          Category:
 					<FormDropDown onChange={(e) => setCurrentCategory(categories[e.target.value - 1])} value={currentCategory.id} name="categories">
 						{categories.map(item => {
 							return <option key={uuidv4()} value={item.id}>{item.name}</option>
 						})}
-					</FormDropDown> */}
+					</FormDropDown>
           Name:
 					<FormItem type="text" placeholder="Product name" name="name" value={product.name} onChange={e =>
 						setName(e.target.value)} />
