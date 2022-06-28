ae12d9ad3939ab88146453cfdb0de39cac8101fd
truearogog <artoxaaa5@gmail.com>
2022-06-28 13:03:23 +0300

    fix

--- GetMalone/Dtos/CreateOrderDto.cs
+++ GetMalone/Dtos/CreateOrderDto.cs
@@ -1,7 +1,8 @@
 ﻿namespace GetMalone.Dtos
 {
-    public class CreateOrderDto : MultiIdDto
+    public class CreateOrderDto
     {
+        public List<int> ProductIds { get; set; }
         public int DeliveryOptionId { get; set; }
     }
 }
